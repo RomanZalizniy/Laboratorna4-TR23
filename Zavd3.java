db72a7380f3d9dd556c1244fe3662d62df358a9e
RomanZalizniy <130225164+RomanZalizniy@users.noreply.github.com>
2023-05-03 20:11:36 +0300

    Add files via upload

---
+++ Zavd3.java
@@ -0,0 +1,13 @@
+public class Zavd3 {
+    public static void main(String[] args) {
+        double[] arr = {1.5, 2.3, 4.7, 3.2, 5.1}; // масив дійсних чисел
+        double sum = 0.0; // змінна, що містить суму елементів масиву
+
+        // Обчислюємо суму елементів масиву
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Сума елементів масиву: " + sum);
+    }
+}
